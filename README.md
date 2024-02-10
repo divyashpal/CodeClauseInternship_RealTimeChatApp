3b6b7442a34e903817924de9f50c4ef9265d5dd6
Divyash Pal <divyashpal4515@gmail.com>
2024-02-10 20:58:39 +0530

    add:sign in page front end done

--- README.md
+++
@@ -1,2 +0,0 @@
-# real_time_chat_app
-This is a real time chat app. Build using React.js, Node.js and Web Socket.
--- client/src/modules/Form/index.js
+++ client/src/modules/Form/index.js
@@ -1,17 +1,23 @@
+import Button from "../../components/Button"
 import Input from "../../components/input"
 
-const Form = () => {
+const Form = ({
+    isSigninPage = true,
+}) => {
     return (
         <div className="bg-white w-[600px] h-[800px] shadow-lg rounded-lg flex flex-col justify-center items-center">
             <div className="text-4xl font-extrabold">
-                welcome
+                welcome {isSigninPage && 'Back'}
             </div>
             <div className="text-xl font-light mb-14">
-                Sign up now to get started
+                {isSigninPage ? "Sign in to explore":"Sign up to get started"}
             </div>
-            <Input label="Full Name" name="name" placeholder="Enter your full name" className="mb-6"/>
+            { !isSigninPage && <Input label="Full Name" name="name" placeholder="Enter your full name" className="mb-6"/>}
             <Input label="Email Address" name="email" placeholder="Enter your email" className="mb-6"/>
-            <Input label="Password" type="password" name="password" placeholder="Enter your password" className="mb-6"/>
+            <Input label="Password" type="password" name="password" placeholder="Enter your password" className="mb-8"/>
+            <Button label={isSigninPage? "Sign in":"Sign up"} className="w-1/2 mb-2 "/>
+            <div>Already have an account? <span className="text-primary cursor-pointer underline">Sign in</span></div>
+
         </div>
     )
 }
